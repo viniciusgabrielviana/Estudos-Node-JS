5ca7b22c1a91cb2b8bbc5b9e995434a0bbc31dd1
Vinicius Gabriel Viana <85412817+viniciusgabrielviana@users.noreply.github.com>
2023-11-26 14:01:27 -0300

    config server, nodemon e inicio de importações

--- API-REST/src/app.js
+++ API-REST/src/app.js
@@ -1,13 +1,30 @@
-const express = require('express')
-const app = express() 
-const port = 3000
+import express from 'express'
+const app = express()
+app.use(express.json())
 
-// criar rota padrão ou raiz
+const times = [
+    {id: 1, nome: 'Grêmio', grupo: 'A'},
+    {id: 2, nome: 'Internacional', grupo: 'A'},
+    {id: 3, nome: 'Barcelona', grupo: 'A'},
+    {id: 4, nome: 'Fluminense', grupo: 'A'},
+    {id: 5, nome: 'Brasil', grupo: 'A'}
+]
+
+function buscarTimePorId (id) {
+    return times.filter( time => time.id == id)
+}
+
 app.get('/', (req, res) => {
     res.send('Hello World')
 })
-
-//escutar a porta
-app.listen(port, () => {
-    console.log(`Servidor Rodando no Endereço http://localhost:${port}`)
+app.get('/times', (req, res) => {
+    res.status(200).send(times)
 })
+app.get('/times/:id', (req, res) => {
+    res.json(buscarTimePorId(req.params.id))
+})
+app.post('/times', (req, res) => {
+    times.push(req.body)
+    res.status(201).send('Time cadastrado com sucesso!')
+})
+export default app
